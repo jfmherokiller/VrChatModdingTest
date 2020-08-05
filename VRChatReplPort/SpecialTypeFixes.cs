85de268eedd9805485aed60831c5e1d5e34ccf05
noah the goodra <peterpan0413@live.com>
2020-08-05 14:26:58 -0500

    add more specialTypes

--- VRChatReplPort/SpecialTypeFixes.cs
+++ VRChatReplPort/SpecialTypeFixes.cs
@@ -20,6 +20,13 @@ namespace PulsarCRepl
             z = myvecZ;
         }
 
+        public SVector3(Vector3 mytransPosition)
+        {
+            x = mytransPosition.x;
+            y = mytransPosition.y;
+            z = mytransPosition.z;
+        }
+
         public float Z
         {
             get => z;
@@ -46,6 +53,90 @@ namespace PulsarCRepl
         public override string ToString()
         {
             return $"{x},{y},{z}";
+        }
+    }
+
+    public struct STransform
+    {
+        private SVector3 Position;
+        private SQuaderton Rotation;
+        
+        public STransform(Transform mytrans)
+        {
+            Position = new SVector3(mytrans.position);
+            Rotation = new SQuaderton(mytrans.rotation);
+        }
+        
+        public SVector3 position
+        {
+            get => Position;
+            set => Position = value;
+        }
+        public SQuaderton rotation
+        {
+            get => Rotation;
+            set => Rotation = value;
+        }
+    }
+
+    public struct SQuaderton
+    {
+        private float w;
+        private float x;
+        private float y;
+        private float z;
+
+        public float W
+        {
+            get => w;
+            set => w = value;
+        }
+
+        public float X
+        {
+            get => x;
+            set => x = value;
+        }
+
+        public float Y
+        {
+            get => y;
+            set => y = value;
+        }
+
+        public float Z
+        {
+            get => z;
+            set => z = value;
+        }
+
+        public SQuaderton(Quaternion mytransRotation)
+        {
+            w = mytransRotation.w;
+            x = mytransRotation.x;
+            y = mytransRotation.y;
+            z = mytransRotation.z;
+        }
+    }
+
+    public struct SGameObject
+    {
+        private STransform _transform;
+        private string _name;
+        public SGameObject(GameObject myGameObject)
+        {
+            _transform = new STransform(myGameObject.transform);
+            _name = myGameObject.name;
+        }
+
+        public string Name
+        {
+            get => _name;
+        }
+
+        public STransform Transform
+        {
+            get => _transform;
         }
     }
 }
