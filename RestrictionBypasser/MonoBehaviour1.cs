8d8c06f43ae08664138c8eb7caf21cb8916a97f6
noah the goodra <peterpan0413@live.com>
2020-11-18 15:34:51 -0600

    add base code for custom blocklist

--- RestrictionBypasser/MonoBehaviour1.cs
+++ RestrictionBypasser/MonoBehaviour1.cs
@@ -9,6 +9,7 @@ using UnhollowerBaseLib;
 using UnhollowerBaseLib.Runtime;
 using UnhollowerRuntimeLib;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 [assembly: MelonInfo(typeof(MonoBehaviour1), "RestrictionKiller", "1.0", "Author Name")]
 [assembly: MelonGame(null, null)]
@@ -17,8 +18,35 @@ namespace RestrictionBypasser
 
     public class MonoBehaviour1 : MelonMod
     {
+        public static string settingsCategory = "CustomComponentBlockList";
+        public static readonly string[] IrcMenuNames = {"IrcUserName", "IrcRealName","IrcNickName","IrcDefaultServer"};
+        public static readonly string[] IrcMenuDisplay = {"Username to use for Irc","Real Name to use for Irc","Nick Name to use for Irc","Default server to use <servername>:<port>:<useSSL>"};
+        public void RegisterModPrefs()
+        {
+            MelonPrefs.RegisterCategory(settingsCategory, "BlockList");
+            MelonPrefs.RegisterString(settingsCategory,"CustomBlocker","");
+
+        }
+        public static IEnumerable<Type> GetTypes()
+        {
+            var blockedclasses = MelonPrefs.GetString(settingsCategory, "CustomBlocker").Split(';').Select(item =>
+            {
+                Type asss = null;
+                try
+                {
+                    asss = Type.GetType(item);
+                }
+                catch (Exception e)
+                {
+                }
+
+                return asss;
+            });
+            return blockedclasses;
+        }
         public override void OnApplicationStart()
         {
+            RegisterModPrefs();
             //FirstPatchSet();
            // SndPatchSet();
             Patcher3();
@@ -30,14 +58,24 @@ namespace RestrictionBypasser
                 typeof(ObjectPublicAbstractSealedDi2StTyHaDi2St1TyUnique)
                     .GetMethod("Method_Public_Static_Void_Component_0"));
            var myrealFunct = typeof(MonoBehaviour1).GetMethod("FakeMethod").MethodHandle.GetFunctionPointer();
-           MelonLogger.Log(realPtr);
-            MelonLogger.Log(myrealFunct);
+           //MelonLogger.Log(realPtr);
+            //MelonLogger.Log(myrealFunct);
             Imports.Hook(realPtr,myrealFunct);
         }
 
         public static void FakeMethod(Component aaaa)
         {
-            return;
+            // var TheTypes = GetTypes();
+            // if(TheTypes == null) return;
+            // if(!TheTypes.Any()) return;
+            // if(aaaa == null) return;
+            // foreach (var type in TheTypes)
+            // {
+            //     if (aaaa.GetType().IsAssignableFrom(type))
+            //     {
+            //         Object.Destroy(aaaa);
+            //     }
+            //}
         }
     }
 }
